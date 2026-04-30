bdb9b92ceb5f2592c5933c8701e6050ffb292787
Rafa-Dev21 <rafaeloliveiradev21@gmail.com>
2026-04-30 10:31:19 -0300

    corrige conflitos git

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express"
 import { register, login } from "../controllers/authController.js"
 
@@ -7,14 +6,4 @@ const router = express.Router()
 router.post("/register", register)
 router.post("/login", login)
 
-=======
-import express from "express"
-import { register, login } from "../controllers/authController.js"
-
-const router = express.Router()
-
-router.post("/register", register)
-router.post("/login", login)
-
->>>>>>> 20a41d8cbec901779cd33f66c1299dd7445aa4da
 export default router
