8433fd4e28bd114560b8754870ddfc58aa3b85fe
Henrique <Henriquesantan_007@Outlook.com>
2026-05-27 17:26:12 -0300

    Arrumando codigo

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ import pedidoRoutes from "./routes/pedido.routes.js";
 import authRoutes from "./routes/authRoutes.js";
 import produtoImagemRoutes from "./routes/produtoImagem.routes.js";
 import cupomRoutes from "./routes/cupom.routes.js";
+import uploadRoutes from "./routes/upload.routes.js";
 
 const app = express()
 
@@ -51,6 +52,7 @@ app.use("/api/pedidos", pedidoRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/produto-imagem", produtoImagemRoutes);
 app.use("/api/cupons", cupomRoutes);
+app.use("/api/upload", uploadRoutes);
 app.get('/', (req, res) => {
   res.send('API rodando 🔥')
 })
