fbafd04bd9bb1d61b7602299d22158d5353dd523
RAFAEL DE OLIVEIRA LIMA <rafael.olima11@senacsp.edu.br>
2026-06-11 08:47:38 -0300

    fix: jwt secret middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,23 +4,26 @@ export const authMiddleware = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization
 
-    // sem token
     if (!authHeader) {
-      return res.status(401).json({ erro: "Token não fornecido" })
+      return res.status(401).json({
+        erro: "Token não fornecido"
+      })
     }
 
-    // formato: Bearer TOKEN
     const token = authHeader.split(" ")[1]
 
-    // verificar token
-    const decoded = jwt.verify(token, "SEGREDO_SUPER_SECRETO")
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "SEGREDO_SUPER_SECRETO"
+    )
 
-    // salvar usuário na req
     req.user = decoded
 
     next()
 
   } catch (err) {
-    return res.status(401).json({ erro: "Token inválido" })
+    return res.status(401).json({
+      erro: "Token inválido"
+    })
   }
 }
